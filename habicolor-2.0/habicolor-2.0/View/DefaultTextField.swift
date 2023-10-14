e70adc09ee52258871632116c0480d7507e0b1e6
Jarret Hoving <jarret.hoving97@gmail.com>
2023-10-14 17:28:18 +0200

    added some composable logic

---
+++ habicolor-2.0/habicolor-2.0/View/DefaultTextField.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultTextField.swift
+//  habicolor-2.0
+//
+//  Created by Jarret Hoving on 08/10/2023.
+//
+
+import SwiftUI
+
+struct DefaultTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DefaultTextField()
+}
